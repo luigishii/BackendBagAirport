ed9d13a13e975fef860ac0116b635e6039b991a8
lkenzo <lkenzo@e-safer.com.br>
2024-09-17 20:34:58 -0300

    adicionando tabela mala

--- src/models.py
+++ src/models.py
@@ -9,3 +9,9 @@ class User(Base):
     email = Column(String, unique=True, index=True)
     cpf = Column(String, unique=True, index=True, nullable=True)
     hashed_password = Column(String)
+    
+class Mala(Base):
+    __tablename__ = 'mala'
+    
+    id = Column(Integer, primary_key=True, autoincrement=True)  # SERIAL é simulado por autoincrement=True
+    rfid = Column(String, unique=True, index=True)
